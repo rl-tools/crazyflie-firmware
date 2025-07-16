d749bc5339c2f75e5a40a570f8dddea2dc8d00f7
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-07-16 12:36:26 -0400

    disabling some safety checks for crazyflie brushless because it would refuse to take off most of the time

--- src/modules/src/stabilizer.c
+++ src/modules/src/stabilizer.c
@@ -334,10 +334,10 @@ static void stabilizerTask(void* param)
 
       // Critical for safety, be careful if you modify this code!
       // Let the supervisor update it's view of the current situation
-      supervisorUpdate(&sensorData, &setpoint, stabilizerStep);
+      // supervisorUpdate(&sensorData, &setpoint, stabilizerStep);
 
       // Let the collision avoidance module modify the setpoint, if needed
-      collisionAvoidanceUpdateSetpoint(&setpoint, &sensorData, &state, stabilizerStep);
+      // collisionAvoidanceUpdateSetpoint(&setpoint, &sensorData, &state, stabilizerStep);
 
       // Critical for safety, be careful if you modify this code!
       // Let the supervisor modify the setpoint to handle exceptional conditions
@@ -350,7 +350,7 @@ static void stabilizerTask(void* param)
       if (areMotorsAllowedToRun) {
         // controlMotors(&control);
       } else {
-        motorsStop();
+        // motorsStop();
       }
 
       // Compute compressed log formats
